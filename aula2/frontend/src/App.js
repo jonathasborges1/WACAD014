95464206faabf2fc9e114e11c1b5857ed6380563
Jonathas Borges <jbc@icomp.ufam.edu.br>
2023-07-11 21:19:42 -0400

    refactor: card products

--- aula2/frontend/src/App.js
+++ aula2/frontend/src/App.js
@@ -4,13 +4,54 @@ import Table from './components/Table';
 import Navbar from './components/Navbar';
 import CardProducts from './components/CardProducts';
 
+
+const dataCards = [
+  {
+    title: "Titulo Card 1",
+    subtitle: "Descricao Card 1",
+    src: "assets/produtos/pc.jpg"
+  },
+  {
+    title: "Titulo do Card 2",
+    subtitle: " Descricao do Card 2",
+    src: "assets/produtos/pc.jpg"
+  },
+  {
+    title: "Titulo do Card 3",
+    subtitle: " Descricao do Card 3",
+    src: "assets/produtos/pc.jpg"
+  },
+  {
+    title: "Titulo do Card 4",
+    subtitle: " Descricao do Card 4",
+    src: "assets/produtos/pc.jpg"
+  },
+  {
+    title: "Titulo do Card 5",
+    subtitle: " Descricao do Card 5",
+    src: "assets/produtos/pc.jpg"
+  },
+  {
+    title: "Titulo do Card 6",
+    subtitle: " Descricao do Card 6",
+    src: "assets/produtos/pc.jpg"
+  },
+]
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Table/>
         <Navbar/>
-        <CardProducts/>
+        {/* <CardProducts/> */}
+
+        {dataCards.map((card, index) => {  
+          return(
+            <CardProducts key={index} src={card.src} title={card.title} subtitle={card.subtitle}  />
+          )
+        })}
+
         {/* <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
--- aula2/frontend/src/components/CardProducts/index.tsx
+++ aula2/frontend/src/components/CardProducts/index.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
-import './style.css';
 
 interface PropsCardProduct {
   children?: React.ReactNode;
+  src?: string;
+  title?: string;
+  subtitle?: string;
 }
 
 const CardProducts: React.FC<PropsCardProduct> = ({ children, ...props }) => {
@@ -10,14 +12,15 @@ const CardProducts: React.FC<PropsCardProduct> = ({ children, ...props }) => {
     <div className='container'>
 
       <div className="row">
+        
         <div className="col-sm-12 col-md-6 col-lg-4">
 
           <div className="card" style={{border: "0px solid red"}} >
 
-            <img className="card-img-top" src="./assets/produtos/pc.jpg" alt="Card image cap" />
+            <img className="card-img-top" src={props.src} alt="Card image cap" />
             <div className="card-body">
-              <h5 className="card-title">Pc Gamer Intel Core i7, 16GB RAM, GTX 1660 6GB, SSD 512GB, Fonte 500w, Gabinete Cooler FAN - Com Wifi</h5>
-              <p className="card-text">O Pc Gamer Intel Core i7 é uma máquina poderosa, ideal para jogos e tarefas que exigem alto desempenho. Equipado com um processador Intel Core i7, oferece um poder de processamento excepcional. Com 16GB de RAM, garante uma execução suave e rápida de aplicativos e jogos</p>
+              <h5 className="card-title">{props.title}</h5>
+              <p className="card-text">{props.subtitle}</p>
             </div>
             <div className="card-footer">
               <small className="text-muted">Last updated 3 mins ago</small>
@@ -26,77 +29,6 @@ const CardProducts: React.FC<PropsCardProduct> = ({ children, ...props }) => {
 
         </div>
 
-        <div className="col-sm-12 col-md-6 col-lg-4">
-          
-          <div className="card" style={{border: "0px solid red"}} >
-
-            <img className="card-img-top" src="./assets/produtos/pc.jpg" alt="Card image cap" />
-            <div className="card-body">
-              <h5 className="card-title">Pc Gamer Intel Core i7, 16GB RAM, GTX 1660 6GB, SSD 512GB, Fonte 500w, Gabinete Cooler FAN - Com Wifi</h5>
-              <p className="card-text">O Pc Gamer Intel Core i7 é uma máquina poderosa, ideal para jogos e tarefas que exigem alto desempenho. Equipado com um processador Intel Core i7, oferece um poder de processamento excepcional. Com 16GB de RAM, garante uma execução suave e rápida de aplicativos e jogos</p>
-            </div>
-            <div className="card-footer">
-              <small className="text-muted">Last updated 3 mins ago</small>
-            </div>
-          </div>
-
-        </div>
-
-        <div className="col-sm-12 col-md-6 col-lg-4">
-          <div className="card" style={{border: "0px solid red"}} >
-
-            <img className="card-img-top" src="./assets/produtos/pc.jpg" alt="Card image cap" />
-            <div className="card-body">
-              <h5 className="card-title">Pc Gamer Intel Core i7, 16GB RAM, GTX 1660 6GB, SSD 512GB, Fonte 500w, Gabinete Cooler FAN - Com Wifi</h5>
-              <p className="card-text">O Pc Gamer Intel Core i7 é uma máquina poderosa, ideal para jogos e tarefas que exigem alto desempenho. Equipado com um processador Intel Core i7, oferece um poder de processamento excepcional. Com 16GB de RAM, garante uma execução suave e rápida de aplicativos e jogos</p>
-            </div>
-            <div className="card-footer">
-              <small className="text-muted">Last updated 3 mins ago</small>
-            </div>
-          </div>
-        </div>
-
-        <div className="col-sm-12 col-md-6 col-lg-4">
-          <div className="card" style={{border: "0px solid red"}} >
-
-            <img className="card-img-top" src="./assets/produtos/pc.jpg" alt="Card image cap" />
-            <div className="card-body">
-              <h5 className="card-title">Pc Gamer Intel Core i7, 16GB RAM, GTX 1660 6GB, SSD 512GB, Fonte 500w, Gabinete Cooler FAN - Com Wifi</h5>
-              <p className="card-text">O Pc Gamer Intel Core i7 é uma máquina poderosa, ideal para jogos e tarefas que exigem alto desempenho. Equipado com um processador Intel Core i7, oferece um poder de processamento excepcional. Com 16GB de RAM, garante uma execução suave e rápida de aplicativos e jogos</p>
-            </div>
-            <div className="card-footer">
-              <small className="text-muted">Last updated 3 mins ago</small>
-            </div>
-          </div>
-        </div>
-
-        <div className="col-sm-12 col-md-6 col-lg-4">
-          <div className="card" style={{border: "0px solid red"}} >
-
-            <img className="card-img-top" src="./assets/produtos/pc.jpg" alt="Card image cap" />
-            <div className="card-body">
-              <h5 className="card-title">Pc Gamer Intel Core i7, 16GB RAM, GTX 1660 6GB, SSD 512GB, Fonte 500w, Gabinete Cooler FAN - Com Wifi</h5>
-              <p className="card-text">O Pc Gamer Intel Core i7 é uma máquina poderosa, ideal para jogos e tarefas que exigem alto desempenho. Equipado com um processador Intel Core i7, oferece um poder de processamento excepcional. Com 16GB de RAM, garante uma execução suave e rápida de aplicativos e jogos</p>
-            </div>
-            <div className="card-footer">
-              <small className="text-muted">Last updated 3 mins ago</small>
-            </div>
-          </div>
-        </div>
-
-        <div className="col-sm-12 col-md-6 col-lg-4">
-          <div className="card" style={{border: "0px solid red"}} >
-
-            <img className="card-img-top" src="./assets/produtos/pc.jpg" alt="Card image cap" />
-            <div className="card-body">
-              <h5 className="card-title">Pc Gamer Intel Core i7, 16GB RAM, GTX 1660 6GB, SSD 512GB, Fonte 500w, Gabinete Cooler FAN - Com Wifi</h5>
-              <p className="card-text">O Pc Gamer Intel Core i7 é uma máquina poderosa, ideal para jogos e tarefas que exigem alto desempenho. Equipado com um processador Intel Core i7, oferece um poder de processamento excepcional. Com 16GB de RAM, garante uma execução suave e rápida de aplicativos e jogos</p>
-            </div>
-            <div className="card-footer">
-              <small className="text-muted">Last updated 3 mins ago</small>
-            </div>
-          </div>
-        </div>
       </div>
 
     </div>
